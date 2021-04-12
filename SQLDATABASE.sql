f22aba26d024f33fb9ebe5af323e0c4bf0f10034
Disha <disha4091@gmail.com>
2021-04-12 22:26:05 +0530

    Inittial commit

---
+++ SQLDATABASE.sql
@@ -0,0 +1,4 @@
+SELECT * FROM cruddatabase.birthdays;
+DELETE FROM cruddatabase.birthdays WHERE id=10 ;
+ALTER TABLE cruddatabase.birthdays
+ADD month varchar(15);
---
+++ server/index.js
@@ -0,0 +1,73 @@
+const express = require('express') ;
+const bodyParser = require('body-parser') ;
+const app = express() ;
+
+const cors = require('cors') ;
+const mysql = require("mysql")
+
+const  db  = {
+    host: "localhost",
+    user: "root",
+    password: "mySQL@2020",
+    database: "cruddatabase",
+    port: 3306,
+    insecureAuth : true
+};      
+app.use(cors()) ;
+app.use(express.json()) ; 
+const con = mysql.createConnection(db);
+
+app.use(bodyParser.urlencoded({extended:true})) ;
+
+app.get("/api/get", (req,res)=>{
+    const sqlSelect = "SELECT * FROM `birthdays` " ;
+    con.query(sqlSelect,(err,result)=>{
+        res.send(result) ;
+    }) ;
+}) ;
+
+app.post("/api/insert",(req,res)=>{
+
+    const Name = req.body.Name ;
+    const DOB = req.body.DOB ;
+    const month = req.body.month ;
+    const sqlInsert = "INSERT INTO `birthdays` (Name , DOB , month) VALUES (?, ? , ?)" ;
+    con.query(sqlInsert,[Name,DOB,month],(err,result)=>{
+        console.log(err) ;
+    }) 
+
+}) ;
+
+app.delete("/api/delete/:Name",(req,res)=>{
+
+    const name = req.params.Name ;
+    
+    const sqlDelete = "DELETE FROM `birthdays` WHERE Name = ? " ;
+    con.query(sqlDelete,name,(err,result)=>{
+        if (err) console.log(err) ;
+    }) 
+
+}) ;
+
+app.put("/api/update",(req,res)=>{
+
+    const name = req.body.Name ;
+    const dob = req.body.DOB ;
+    const month = req.body.month ;
+    const sqlUpdate = "UPDATE `birthdays` SET  DOB = ?,month = ? WHERE Name = ? " ;
+    con.query(sqlUpdate,[dob,month,name],(err,result)=>{
+        if (err) console.log(err) ;
+    }) 
+
+}) ;
+/*app.get("/",(req,res)=>{
+    const sqlInsert = "INSERT INTO `birthdays` (Name , DOB) VALUES ('xyz', '12/2/2001')" ;
+    con.query(sqlInsert, (err, result)=>{
+        res.send("hi disha") ;
+    });
+
+}) ;*/
+
+app.listen(3001, ()=>{
+    console.log("Running on port 3001");
+})
